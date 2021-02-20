e1b4c04534f3c8de4f4bddd5ea1e676d5614fc62
sanvega9 <59941870+sanvega9@users.noreply.github.com>
2021-02-19 22:56:28 -0700

    Add files via upload

---
+++ bellvega.py
@@ -0,0 +1,9 @@
+print("Hola chico")
+nombre_de_usuario = input("introduzca su nombre de usuario:")
+print("nombre de usuario es "+nombre_de_usuario)
+
+la_contraseña = input("introducir la contraseña:")
+print("Tu contraseña es "+la_contraseña)
+
+
+
